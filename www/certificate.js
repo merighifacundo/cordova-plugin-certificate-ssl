a216494646b20933208615339014015873677f83
Facundo Merighi <merighifacundo@gmail.com>
2018-08-31 05:08:18 -0400

    Adding certificate

--- www/certificate.js
+++ www/certificate.js
@@ -1,14 +1,13 @@
-cordova.define("cordova-plugin-certificate.Certificate", function(require, exports, module) {
-    var exec = require('cordova/exec');
-    
-    module.exports = {
-        isCertificate: function(onSuccess, onError) {
-            exec(onSuccess, onError, 'Certificate', 'isCertificate', []);
-        },
-        isPinCertificate: function(onSuccess, onError) {
-            exec(onSuccess, onError, 'Certificate', 'isCertificate', []);
-        }
-    };
-    
-});
+
+var exec = require('cordova/exec');
+
+module.exports = {
+    isCertificate: function(onSuccess, onError) {
+        exec(onSuccess, onError, 'Certificate', 'isCertificate', []);
+    },
+    isPinCertificate: function(onSuccess, onError) {
+        exec(onSuccess, onError, 'Certificate', 'isCertificate', []);
+    }
+};
+
     
